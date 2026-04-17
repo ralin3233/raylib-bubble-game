5f7595dc877d3c70dbdabb88d530bc4bff191449
YuChenLin <linrain0323@gmail.com>
2026-04-17 17:06:18 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "raylib.h"
+int main() {
+    InitWindow(800, 450, "Raylib OOP");
+    SetTargetFPS(60);
+
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
